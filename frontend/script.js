5da7806a43f4830892686e321a621a6cced6c987
codec-onion <bonnetmatthieu86@gmail.com>
2024-12-10 20:23:44 +0100

    add generation of the 2 stars numbers and display draw

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 const numberOfDraws = document.querySelector("#number_of_draws")
 const startButton = document.querySelector(".start")
+const displayDraw = document.querySelector(".result")
 
 let totalNumbers = 5
 let totalStars = 2
@@ -8,24 +9,39 @@ let starsNumbers = []
 
 const createRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + 1)
 
-const createClassicDraw = () => {
+const createDraw = () => {
+  let min = 1
+  let max = 51
   for (let i = 0; i < totalNumbers; i++) {
-    let min = 1
-    let max = 51
     let randomNumber = createRandomNumber(min, max)
     while (classicNumbers.includes(randomNumber)) {
       randomNumber = createRandomNumber(min, max)
     }
     classicNumbers.push(randomNumber)
   }
-  return classicNumbers
+  max = 12
+  for (let i = 0; i < totalStars; i++) {
+    let randomNumber = createRandomNumber(min, max)
+    while (starsNumbers.includes(randomNumber)) {
+      randomNumber = createRandomNumber(min, max)
+    }
+    starsNumbers.push(randomNumber)
+  }
+  classicNumbers.sort((a, b) => a - b)
+  starsNumbers.sort((a, b) => a - b)
+
+  return classicNumbers.concat(starsNumbers).join(" ")
 }
 
-const showClassicDraw = () => {
-  console.log(createClassicDraw())
-  classicNumbers = []
+const showDraw = () => {
+  return createDraw()
 }
 
 startButton.addEventListener("click", (e) => {
-  showClassicDraw()
+  let p = document.createElement("p")
+  let content = showDraw()
+  classicNumbers = []
+  starsNumbers = []
+  p.innerText = content
+  displayDraw.appendChild(p)
 })
